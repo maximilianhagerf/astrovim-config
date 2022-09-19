e9e23726425c5fea734b6470ded58787b17a1ff7
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-19 16:09:32 -0400

    feaf: new folder for snippets + moved luasnip to luasnip.lua

---
+++ luasnip.lua
@@ -0,0 +1,11 @@
+  -- LuaSnip Options
+ return {
+    -- Add paths for including more VS Code style snippets in luasnip
+    vscode_snippet_paths = {
+      "./lua/user/snippets",
+    },
+    -- Extend filetypes
+    filetype_extend = {
+      javascript = { "javascriptreact" },
+    },
+  }
