4d54928040d043cfe6a24859a7d5436d420c8051
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-19 19:15:55 -0400

    feat: spaceduck is the theme now + cleanup

--- init.lua
+++ init.lua
@@ -20,7 +20,8 @@ local config = {
   },
 
   -- Set colorscheme to use
-  colorscheme = "default_theme",
+  colorscheme = "spaceduck",
+  -- colorscheme = "default_theme",
 
   -- Override highlight groups in any theme
   highlights = {
@@ -147,33 +148,6 @@ local config = {
     },
   },
 
-  -- This function is run last and is a good place to configuring
-  -- augroups/autocommands and custom filetypes also this just pure lua so
-  -- anything that doesn't fit in the normal config locations above can go here
-  polish = function()
-    -- Set key binding
-    -- Set autocommands
-    vim.api.nvim_create_augroup("packer_conf", { clear = true })
-    vim.api.nvim_create_autocmd("BufWritePost", {
-      desc = "Sync packer after modifying plugins.lua",
-      group = "packer_conf",
-      pattern = "plugins.lua",
-      command = "source <afile> | PackerSync",
-    })
-
-    -- Set up custom filetypes
-    -- vim.filetype.add {
-    --   extension = {
-    --     foo = "fooscript",
-    --   },
-    --   filename = {
-    --     ["Foofile"] = "fooscript",
-    --   },
-    --   pattern = {
-    --     ["~/%.config/foo/.*"] = "fooscript",
-    --   },
-    -- }
-  end,
 }
 
 return config
