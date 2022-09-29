6a04a1f031eb25e65a108175481af7fb69c3f729
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-29 11:37:34 -0400

    feat(which): separate config file, no border more padding

---
+++ plugins/which-key.lua
@@ -0,0 +1,24 @@
+return {
+  plugins = {
+    marks = true,
+    registers = true,
+    spelling = {
+      enabled = true,
+      suggestions = 30,
+    },
+    presets = {
+      operators = false,
+      motions = true,
+      text_objects = true,
+      windows = true,
+      nav = true,
+      z = true,
+      g = true,
+    },
+  },
+  window = {
+    border = "none",
+    margin = { 0, 0, 1, 0 },
+    padding = { 4, 2, 4, 2 },
+  },
+}
