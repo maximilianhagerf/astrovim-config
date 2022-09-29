0638f4df0778172fac2e08789a4d8bcc13050529
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-29 11:55:34 -0400

    feat(telescope): bigger and better!

---
+++ plugins/telescope.lua
@@ -0,0 +1,29 @@
+local actions = require "telescope.actions"
+return {
+  defaults = {
+    prompt_prefix = " ",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.6,
+        results_width = 0.8,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.95,
+      height = 0.90,
+      preview_cutoff = 100,
+    },
+    mappings = {
+      i = {
+        ["<C-h>"] = actions.select_horizontal,
+      },
+
+      n = {
+        ["<C-h>"] = actions.select_horizontal,
+      },
+    },
+  },
+}
