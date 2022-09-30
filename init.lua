4c3c8fe252cf307736a82e0e1ff92661bc79db37
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-30 01:23:19 -0400

    fix: commented out highlights

--- init.lua
+++ init.lua
@@ -24,17 +24,17 @@ local config = {
   -- colorscheme = "default_theme",
 
   -- Override highlight groups in any theme
-  highlights = {
-    -- duskfox = { -- a table of overrides/changes to the default
-    --   Normal = { bg = "#000000" },
-    -- },
-    default_theme = function(highlights) -- or a function that returns a new table of colors to set
-      local C = require "default_theme.colors"
-
-      highlights.Normal = { fg = C.fg, bg = C.bg }
-      return highlights
-    end,
-  },
+  -- highlights = {
+  --   -- duskfox = { -- a table of overrides/changes to the default
+  --   --   Normal = { bg = "#000000" },
+  --   -- },
+  --   default_theme = function(highlights) -- or a function that returns a new table of colors to set
+  --     local C = require "default_theme.colors"
+  --
+  --     highlights.Normal = { fg = C.fg, bg = C.bg }
+  --     return highlights
+  --   end,
+  -- },
 
   -- Default theme configuration
   default_theme = {
