d300ebe5de4a552eadf80135ac886b4d509a312f
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-30 01:17:49 -0400

    feat(tabs): override default config

---
+++ plugins/bufferline.lua
@@ -0,0 +1,25 @@
+return {
+  options = {
+    offsets = {
+      { filetype = "NvimTree", text = "", padding = 0 },
+      { filetype = "neo-tree", text = "", padding = 0 },
+      { filetype = "Outline", text = "", padding = 0 },
+    },
+    buffer_close_icon = "",
+    modified_icon = "",
+    close_icon = "",
+    show_close_icon = false,
+    left_trunc_marker = "",
+    right_trunc_marker = "",
+    max_name_length = 14,
+    max_prefix_length = 13,
+    tab_size = 20,
+    show_tab_indicators = false,
+    enforce_regular_tabs = false,
+    view = "multiwindow",
+    show_buffer_close_icons = false,
+    separator_style = "thin",
+    always_show_bufferline = false,
+    diagnostics = false,
+  },
+}
