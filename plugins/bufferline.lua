1960e2f61e2ed3fdb89f0eed7dbd3f96de2d807b
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-10-01 02:21:09 -0400

    style(tabs): fixed highlights and settings

--- plugins/bufferline.lua
+++ plugins/bufferline.lua
@@ -1,4 +1,54 @@
 return {
+  highlights = {
+    buffer_selected = {
+      italic = false,
+      bold = false,
+      fg = "#00a3cc"
+    },
+	  diagnostic_selected = { italic = false },
+	  hint_selected = { italic = false },
+	  pick_selected = { italic = false },
+	  pick_visible = { italic = false },
+	  pick = { italic = false },
+    fill = { bg = "NONE"},
+    background = {
+      bg = "NONE"
+    },
+    tab = {
+      fg = "NONE",
+      bg = "NONE",
+    },
+    tab_close = {
+      fg = "NONE",
+      bg = "NONE"
+    },
+    close_button = {
+      bg = "NONE"
+    },
+    close_button_selected = {
+      fg = "#00a3cc"
+    },
+    separator_selected = {
+      fg = "#0f111b",
+      bg = "NONE"
+    },
+    separator_visible = {
+      fg = "#0f111b",
+      bg = "NONE"
+    },
+    separator = {
+      fg = "#0f111b",
+      bg = "NONE"
+    },
+    indicator_selected = {
+      fg = "NONE",
+      bg = "NONE"
+    },
+    offset_separator = {
+      fg = "NONE",
+      bg = "NONE",
+    },
+  },
   options = {
     offsets = {
       { filetype = "NvimTree", text = "", padding = 0 },
@@ -14,12 +64,18 @@ return {
     max_name_length = 14,
     max_prefix_length = 13,
     tab_size = 20,
-    show_tab_indicators = false,
+    show_tab_indicators = true,
     enforce_regular_tabs = false,
     view = "multiwindow",
-    show_buffer_close_icons = false,
-    separator_style = "thin",
+    show_buffer_close_icons = true,
+    -- separator_style = {" ", " "},
+    separator_style = "slant",
     always_show_bufferline = false,
     diagnostics = false,
+    -- hover = {
+    --   enabled = true,
+    --   delay = 200,
+    --   reveal = {'close'}
+    -- }
   },
 }
