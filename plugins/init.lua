1046cfd624d742efe7af3850c98a4ab1ee2a6195
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-10-04 15:27:31 -0400

    feat: vim multi cursors and TS playground

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,25 +1,6 @@
 -- Configure plugins
 return {
-  'pineapplegiant/spaceduck',
-    -- You can disable default plugins as follows:
-    -- ["goolord/alpha-nvim"] = { disable = true },
-
-    -- You can also add new plugins here as well:
-    -- Add plugins, the packer syntax without the "use"
-    -- { "andweeb/presence.nvim" },
-    -- {
-    --   "ray-x/lsp_signature.nvim",
-    --   event = "BufRead",
-    --   config = function()
-    --     require("lsp_signature").setup()
-    --   end,
-    -- },
-
-    -- We also support a key value style plugin definition similar to NvChad:
-    -- ["ray-x/lsp_signature.nvim"] = {
-    --   event = "BufRead",
-    --   config = function()
-    --     require("lsp_signature").setup()
-    --   end,
-    -- },
+  "pineapplegiant/spaceduck",
+  "mg979/vim-visual-multi",
+  "nvim-treesitter/playground",
 }
